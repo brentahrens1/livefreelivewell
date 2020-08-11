283b65318bc41c62f37dff2e846e42059ff2610a
Brent Ahrens <cidida@gmail.com>
2020-08-11 06:28:23 -0700

    adds basic nav desktop structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './sass/_base.scss'
+import './sass/_base.scss';
+
+import Nav from './components/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../sass/_nav.scss'
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <div className="nav__logo">
+                <h1>Logo</h1>
+            </div>
+            <ul className="nav__list">
+                <li className="nav__list-item"><a href="#">Home</a></li>
+                <li className="nav__list-item"><a href="#">Programs</a>
+                    <ul className="nav__dropdown">
+                        <li className="nav__dropdown-link"><a href="#">Kettlebell Club</a></li>
+                        <li className="nav__dropdown-link"><a href="#">Personal Training</a></li>
+                        <li className="nav__dropdown-link"><a href="#">Coaching</a></li>
+                    </ul>
+                </li>
+                <li className="nav__list-item"><a href="#">About</a></li>
+                <li className="nav__list-item"><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
