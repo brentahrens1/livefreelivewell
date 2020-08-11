2f349c5f4964fa658559ff0a4c78582a9ac75e0e
Brent Ahrens <cidida@gmail.com>
2020-08-11 09:35:55 -0700

    adds overlay functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../sass/_nav.scss'
 
 const Nav = () => {
+    const [ isOpen, setIsOpen ] = useState(false)
     return (
         <div className="nav">
             <div className="nav__logo">
@@ -9,7 +10,7 @@ const Nav = () => {
             </div>
             <ul className="nav__list">
                 <li className="nav__list-item"><a href="#">Programs</a>
-                    <ul className="nav__dropdown dropdown-spot">
+                    <ul className="nav__dropdown">
                         <li className="nav__dropdown-link"><a href="#">Kettlebell Club</a></li>
                         <li className="nav__dropdown-link"><a href="#">Personal Training</a></li>
                         <li className="nav__dropdown-link"><a href="#">Coaching</a></li>
@@ -18,6 +19,30 @@ const Nav = () => {
                 <li className="nav__list-item"><a href="#">About</a></li>
                 <li className="nav__list-item"><a href="#">Contact</a></li>
             </ul>
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <div className="hamburger__bar" />
+                <div className="hamburger__bar" />
+                <div className="hamburger__bar" />
+            </div>
+            <div className={`overlay ${isOpen ? "open" : "hide"}`}>
+                <div className="overlay__close">
+                    <div className="overlay__close-icon" onClick={() => setIsOpen(!isOpen)}>
+                        <div className="overlay__close-bar" />
+                        <div className="overlay__close-bar" />
+                    </div>
+                </div>
+                <ul className="overlay__list">
+                    <li className="overlay__list-item"><a href="#">Programs</a>
+                        <ul className="overlay__dropdown">
+                            <li className="overlay__dropdown-link"><a href="#">Kettlebell Club</a></li>
+                            <li className="overlay__dropdown-link"><a href="#">Personal Training</a></li>
+                            <li className="overlay__dropdown-link"><a href="#">Coaching</a></li>
+                        </ul>
+                    </li>
+                    <li className="overlay__list-item"><a href="#">About</a></li>
+                    <li className="overlay__list-item"><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
