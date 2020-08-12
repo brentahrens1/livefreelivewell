72eff2acca3b323bdf8af02067a8db1969e3bd96
brentahrens1 <cidida@gmail.com>
2020-08-12 14:43:38 -0700

    adds programs routes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import '../../sass/_home.scss'
 
+import kettleBellClub from '../../assets/images/test.jpg'
+
+import { programs } from '../../const/programs'
+ 
 const Home = () => {
     return (
         <div className="home">
             <div className="home__card">
                 <div className="home__card-message-inner">
                     <div className="home__card-message-inner-content">
-                        <h1>Philosophy</h1>
-                        <p>This is about gains. This is about getting shredded. This is about getting that 12 pack ready for the beach. That's my mission. This ain't no 6 minute buns. This is sweat this is blood and yes my friends this is tears. Are you prepared to feel the burn? Are you? Well then. Let's go. Let's take this epic shred fest to the streets!</p>
-                        <h4>Warning: You're guns will be deadly</h4>
+                        <h1>Let's get shredded!</h1>
+                        <h4>Philosophy</h4>
                     </div>
                 </div>
             </div>
+            {
+                programs.map((program, idx) =>
+                    <div className="home__programs">
+                        <h1>{program.title}</h1>
+                        <div className="home__programs-image">
+                            <img src={program.img} alt={program.title} />
+                        </div>
+                        <div className="home__programs-content">
+                            <p>{program.description}</p>
+                            <button>{program.buttonTxt}</button>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
---
+++ src/const/programs.js
@@ -0,0 +1,25 @@
+import kettlebellClub from '../assets/images/test.jpg'
+
+export const programs = [
+    {
+        title: "Training",
+        img: kettlebellClub,
+        description: 'The world-renowned nootropic supplement. Alpha BRAIN® supports memory, focus, and cognitive processing speed. It’s the ultimate way to get “in the zone”—the flow state where you’re at the most creative, productive, and free of distractions.†︎ Learn More',
+        link: '',
+        buttonTxt: 'Sign up now'
+    },
+    {
+        title: "Kettlebell Club",
+        img: kettlebellClub,
+        description: 'The world-renowned nootropic supplement. Alpha BRAIN® supports memory, focus, and cognitive processing speed. It’s the ultimate way to get “in the zone”—the flow state where you’re at the most creative, productive, and free of distractions.†︎ Learn More',
+        link: '',
+        buttonTxt: 'Join the club'
+    },
+    {
+        title: "Coaching",
+        img: kettlebellClub,
+        description: 'The world-renowned nootropic supplement. Alpha BRAIN® supports memory, focus, and cognitive processing speed. It’s the ultimate way to get “in the zone”—the flow state where you’re at the most creative, productive, and free of distractions.†︎ Learn More',
+        link: '',
+        buttonTxt: 'Sign up now'
+    },
+]
