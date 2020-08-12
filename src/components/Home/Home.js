e0ec657126d2dac0be7d8531e37f4abd14f35879
Brent Ahrens <cidida@gmail.com>
2020-08-12 05:56:17 -0700

    begins home section layout and styling

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../sass/_home.scss'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__card">
+                <div className="home__card-row">
+                    <div className="home__card-message">
+                        <div className="home__card-message-inner">
+                            <h1>Philosophy</h1>
+                            <p>This is about gains. This is about getting shredded. This is about getting that 12 pack ready for the beach. That's my mission. This ain't no 6 minute buns. This is sweat this is blood and yes my friends this is tears. Are you prepared to feel the burn? Are you? Well then. Let's go. Let's take this epic shred fest to the streets!</p>
+                            <h4>Warning: You're guns will be deadly</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
