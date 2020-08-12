c675077732532e9a36c9b25fb8fa504842bedc8b
Brent Ahrens <cidida@gmail.com>
2020-08-12 10:33:20 -0700

    reworks home layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,11 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home__card">
-                <div className="home__card-row">
-                    <div className="home__card-message">
-                        <div className="home__card-message-inner">
-                            <h1>Philosophy</h1>
-                            <p>This is about gains. This is about getting shredded. This is about getting that 12 pack ready for the beach. That's my mission. This ain't no 6 minute buns. This is sweat this is blood and yes my friends this is tears. Are you prepared to feel the burn? Are you? Well then. Let's go. Let's take this epic shred fest to the streets!</p>
-                            <h4>Warning: You're guns will be deadly</h4>
-                        </div>
+                <div className="home__card-message-inner">
+                    <div className="home__card-message-inner-content">
+                        <h1>Philosophy</h1>
+                        <p>This is about gains. This is about getting shredded. This is about getting that 12 pack ready for the beach. That's my mission. This ain't no 6 minute buns. This is sweat this is blood and yes my friends this is tears. Are you prepared to feel the burn? Are you? Well then. Let's go. Let's take this epic shred fest to the streets!</p>
+                        <h4>Warning: You're guns will be deadly</h4>
                     </div>
                 </div>
             </div>
