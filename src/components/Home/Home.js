100ef46ba661fb76b96adff30e9e7cba4a95bbc2
Brent Ahrens <cidida@gmail.com>
2020-10-02 19:28:20 -0700

    adds hover effects works on responsiveness

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import '../../sass/_home.scss'
 import { Link } from 'react-router-dom'
 
 import { programs } from '../../const/programs'
+
+import testImg from '../../assets/images/programs/coaching-min.png'
  
 const Home = () => {
     return (
@@ -19,11 +21,11 @@ const Home = () => {
                 programs.map((program, idx) =>
                     <Link to={program.path}>
                         <div className="home__programs">
-                            <h1>{program.title}</h1>
                             <div className="home__programs-image">
                                 <img src={program.img} alt={program.title} />
                             </div>
                             <div className="home__programs-content">
+                                <h1>{program.title}</h1>
                                 <p>{program.description}</p>
                                 <button>{program.buttonTxt}</button>
                             </div>
